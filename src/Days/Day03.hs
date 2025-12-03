d75d019ade0bcc69375939ffdfa9d648ae216326
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2025-12-03 15:37:57 +0100

    feat: add solutions for day 03

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -1,9 +1,32 @@
 module Days.Day03 (solveA, solveB) where
 
+import Data.Attoparsec.Text
+import Data.Char (isDigit, digitToInt)
 import Data.Text (Text)
+import qualified Data.Text as T
+import Parse (mustParse)
+
+type Joltage = Int
+
+joltages :: Parser [Joltage]
+joltages = map digitToInt <$> many1 (satisfy isDigit)
+
+banks :: Parser [[Joltage]]
+banks = joltages `sepBy` endOfLine
+
+outputJoltageN :: Int -> [Joltage] -> Joltage
+outputJoltageN n xs = go 0 n xs
+  where
+    go v 0 _ = v
+    go v i xs =
+      let maxN = maximum $ Prelude.take (length xs - (i - 1)) xs
+          xs' = drop 1 $ dropWhile (< maxN) xs
+          v' = v + maxN * (10 ^ (i - 1))
+          i' = i - 1
+       in go v' i' xs'
 
 solveA :: Text -> Text
-solveA input = input
+solveA = T.show . sum . map (outputJoltageN 2) . mustParse banks
 
 solveB :: Text -> Text
-solveB input = input
+solveB = T.show . sum . map (outputJoltageN 12) . mustParse banks
